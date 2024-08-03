59bbeb5c3893bddc28136a6b75860ec860d98fcb
Сергей Политаев <ronin@sonikelf.ru>
2024-08-03 17:16:41 +0300

    Implement fetching a single property data from the server

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,26 @@
 'use client';
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import { fetchProperty } from '@/utils/requests';
+
 const PropertyPage = () => {
+	const { id } = useParams();
+	const [isLoading, setIsLoading] = useState(false);
+	const [property, setProperty] = useState(null);
+	useEffect(() => {
+		const fetchPropertyData = async () => {
+			if (!id) return;
+			try {
+				const property = await fetchProperty(id);
+				setProperty(property);
+			} catch (error) {
+				console.error('Error fetching property:', error);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+		if (property === null) fetchPropertyData();
+	}, [property, isLoading]);
 	return <div>PropertyPage</div>;
 };
 
--- utils/requests.js
+++ utils/requests.js
@@ -15,5 +15,22 @@ const fetchProperties = async () => {
 		return [];
 	}
 };
+const fetchProperty = async (id) => {
+	try {
+		if (!apiDomain) {
+			return null;
+		}
+		const res = await fetch(
+			`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties/${id}`
+		);
+		if (!res.ok) {
+			throw new Error('Failed to fetch data');
+		}
+		return res.json();
+	} catch (error) {
+		console.log(error);
+		return null;
+	}
+};
 
-export { fetchProperties };
+export { fetchProperties, fetchProperty };
