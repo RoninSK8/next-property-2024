2d30e0924a293cbbb32472c93b05b28157e0ee06
Сергей Политаев <ronin@sonikelf.ru>
2024-08-04 00:42:37 +0300

    Set up Next Auth and Session Provider Wrapper

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import '@/assets/styles/globals.css';
 import '@/components/Navbar';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import AuthProvider from '@/components/AuthProvider';
 
 export const metadata = {
 	title: 'Project title',
@@ -11,13 +12,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
 	return (
-		<html lang="en">
-			<body>
-				<Navbar />
-				<main>{children}</main>
-				<Footer />
-			</body>
-		</html>
+		<AuthProvider>
+			<html lang="en">
+				<body>
+					<Navbar />
+					<main>{children}</main>
+					<Footer />
+				</body>
+			</html>
+		</AuthProvider>
 	);
 };
 
