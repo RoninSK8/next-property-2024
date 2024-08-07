233e04a3c00240ea2900fc03f5e8cda5c997e5ed
Сергей Политаев <ronin@sonikelf.ru>
2024-08-07 22:05:10 +0300

    Implement getting user and submitting form to database

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,6 @@
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
+import { getSessionUser } from '@/utils/getSessionUser';
 
 // GET /api/properties
 export const GET = async (request) => {
@@ -19,6 +20,14 @@ export const GET = async (request) => {
 
 export const POST = async (request) => {
 	try {
+		await connectDB();
+
+		const sessionUser = await getSessionUser();
+		if (!sessionUser || !sessionUser.userId) {
+			return new Response('User Id is required', { status: 401 });
+		}
+		const { userId } = sessionUser;
+
 		const formData = await request.formData();
 		const amenities = formData.getAll('amenities');
 		const images = formData
@@ -26,6 +35,7 @@ export const POST = async (request) => {
 			.filter((image) => image.name !== '');
 
 		const propertyData = {
+			owner: userId,
 			type: formData.get('type'),
 			name: formData.get('name'),
 			description: formData.get('description'),
@@ -49,14 +59,19 @@ export const POST = async (request) => {
 				email: formData.get('seller_info.email'),
 				phone: formData.get('seller_info.phone'),
 			},
-			images,
+			// images,
 		};
 
-		console.log(propertyData);
+		const newProperty = new Property(propertyData);
+		await newProperty.save();
 
-		return new Response(JSON.stringify({ message: 'Success' }), {
-			status: 200,
-		});
+		return Response.redirect(
+			`${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`
+		);
+
+		// return new Response(JSON.stringify({ message: 'Success' }), {
+		// 	status: 200,
+		// });
 	} catch (error) {
 		return new Response('Failed to add property', { status: 500 });
 	}
