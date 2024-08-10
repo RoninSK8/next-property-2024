cad413ab70250cfd74c289469d18cf4e493d54c0
Сергей Политаев <ronin@sonikelf.ru>
2024-08-10 16:12:48 +0300

    Implement deleting property from profile page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -38,7 +38,31 @@ const ProfilePage = () => {
 		}
 	}, [session]);
 
-	const handleDeleteProperty = () => {};
+	const handleDeleteProperty = async (propertyId) => {
+		const confirmed = window.confirm(
+			'Are you sure you want to delete this property?'
+		);
+
+		if (!confirmed) return;
+
+		try {
+			const res = await fetch(`api/properties/${propertyId}`, {
+				method: 'DELETE',
+			});
+			if (res.status === 200) {
+				const updatedProperties = properties.filter(
+					(property) => property._id !== propertyId
+				);
+				setProperties(updatedProperties);
+				alert('Property deleted');
+			} else {
+				alert('Failed to delete property');
+			}
+		} catch (error) {
+			console.log(error);
+			alert('Failed to delete property');
+		}
+	};
 
 	return (
 		<section className="bg-blue-50">
