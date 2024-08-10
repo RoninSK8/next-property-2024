b41515d25f3fe018ad0235b067f0e446d1479e7c
Сергей Политаев <ronin@sonikelf.ru>
2024-08-10 18:46:14 +0300

    Add PUT API route for updating a property

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -54,3 +54,69 @@ export const DELETE = async (request, { params }) => {
 		});
 	}
 };
+
+// PUT /api/properties/:id
+export const PUT = async (request, { params }) => {
+	try {
+		await connectDB();
+
+		const sessionUser = await getSessionUser();
+		if (!sessionUser || !sessionUser.userId) {
+			return new Response('User Id is required', { status: 401 });
+		}
+		const { id } = params;
+		const { userId } = sessionUser;
+
+		const formData = await request.formData();
+		const amenities = formData.getAll('amenities');
+
+		// Get property to update
+		const existingProperty = await Property.findById(id);
+
+		if (!existingProperty) {
+			return new Response('Property does not exist', { status: 404 });
+		}
+
+		// Verify ownership
+		if (existingProperty.owner.toString() !== userId) {
+			return new Response('unauthorized', { status: 401 });
+		}
+
+		const propertyData = {
+			owner: userId,
+			type: formData.get('type'),
+			name: formData.get('name'),
+			description: formData.get('description'),
+			location: {
+				street: formData.get('location.street'),
+				city: formData.get('location.city'),
+				state: formData.get('location.state'),
+				zipcode: formData.get('location.zipcode'),
+			},
+			beds: formData.get('beds'),
+			baths: formData.get('baths'),
+			square_feet: formData.get('square_feet'),
+			amenities,
+			rates: {
+				weekly: formData.get('rates.weekly'),
+				monthly: formData.get('rates.monthly'),
+				nightly: formData.get('rates.nightly.'),
+			},
+			seller_info: {
+				name: formData.get('seller_info.name'),
+				email: formData.get('seller_info.email'),
+				phone: formData.get('seller_info.phone'),
+			},
+		};
+
+		// Update property in database
+		const updatedProperty = await Property.findByIdAndUpdate(id, propertyData);
+
+		return new Response(JSON.stringify(updatedProperty), {
+			status: 200,
+		});
+	} catch (error) {
+		console.error(error);
+		return new Response('Failed to update property', { status: 500 });
+	}
+};
