685158ec2ee8d64e1e0c4818bd0558c11dcd1930
Сергей Политаев <ronin@sonikelf.ru>
2024-08-10 16:34:20 +0300

    Add React-Toastify alerts for deleting a property

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import Spinner from '@/components/Spinner';
 import profileDefault from '@/assets/images/profile.png';
+import { toast } from 'react-toastify';
 
 const ProfilePage = () => {
 	const { data: session } = useSession();
@@ -54,13 +55,13 @@ const ProfilePage = () => {
 					(property) => property._id !== propertyId
 				);
 				setProperties(updatedProperties);
-				alert('Property deleted');
+				toast.success('Property deleted');
 			} else {
-				alert('Failed to delete property');
+				toast.error('Failed to delete property');
 			}
 		} catch (error) {
 			console.log(error);
-			alert('Failed to delete property');
+			toast.error('Failed to delete property');
 		}
 	};
 
