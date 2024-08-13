cf96004d6877ca720a9c5b583c2b069e3a626c09
Сергей Политаев <ronin@sonikelf.ru>
2024-08-13 23:06:34 +0300

    Add Bookmark Button toggle and status

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -10,6 +10,40 @@ const BookmarkButton = ({ property }) => {
 	const userId = session?.user?.id;
 
 	const [isBoobmarked, setIsBookmarked] = useState(false);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		if (!userId) {
+			setLoading(false);
+			return;
+		}
+
+		const checkBookmarkStatus = async () => {
+			try {
+				const res = await fetch('/api/bookmarks', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({
+						propertyId: property._id,
+					}),
+				});
+
+				if (res.status === 200) {
+					const data = await res.json();
+					setIsBookmarked(data.isBookmarked);
+				}
+			} catch (error) {
+				console.error(error);
+				toast.error('Something went wrong');
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		checkBookmarkStatus();
+	}, [property._id, userId]);
 
 	const handleClick = async () => {
 		console.log(userId);
@@ -40,7 +74,16 @@ const BookmarkButton = ({ property }) => {
 		}
 	};
 
-	return (
+	if (loading) return <p className="text-center">Loading...</p>;
+
+	return isBoobmarked ? (
+		<button
+			onClick={handleClick}
+			className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+		>
+			<FaBookmark className="mr-2"></FaBookmark> Remove Bookmark
+		</button>
+	) : (
 		<button
 			onClick={handleClick}
 			className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
