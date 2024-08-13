5b06ce4f19540ae39a60d6f7da4a54408da2553d
Сергей Политаев <ronin@sonikelf.ru>
2024-08-14 00:50:25 +0300

    Add Loading message to Bookmark button

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -73,7 +73,15 @@ const BookmarkButton = ({ property }) => {
 		}
 	};
 
-	if (loading) return <p className="text-center">Loading...</p>;
+	if (loading)
+		return (
+			<button
+				disabled
+				className="bg-blue-200 hover:bg-blue-300 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+			>
+				<FaBookmark className="mr-2"></FaBookmark> Loading...
+			</button>
+		);
 
 	return isBookmarked ? (
 		<button
