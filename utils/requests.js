4313a19ae854c79108c66f255d6a2b2222dfb3f7
Сергей Политаев <ronin@sonikelf.ru>
2024-08-16 23:07:59 +0300

    Fix problem with build by removing try catch

--- utils/requests.js
+++ utils/requests.js
@@ -2,26 +2,22 @@ const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
 // Fetch all properties
 async function fetchProperties({ showFeatured = false } = {}) {
-	try {
-		// Handle the case where the domain is not available yet
-		if (!apiDomain) {
-			return [];
-		}
-
-		const res = await fetch(
-			`${apiDomain}/properties${showFeatured ? '/featured' : ''}`,
-			{ cache: 'no-store' }
-		);
-
-		if (!res.ok) {
-			throw new Error('Failed to fetch data');
-		}
-
-		return res.json();
-	} catch (error) {
-		console.log(error);
+	// Handle the case where the domain is not available yet
+	if (!apiDomain) {
 		return [];
 	}
+
+	const res = await fetch(
+		`${apiDomain}/properties${showFeatured ? '/featured' : ''}`,
+		{ cache: 'no-store' }
+	);
+
+	if (!res.ok) {
+		console.log(error);
+		throw new Error('Failed to fetch data');
+	}
+
+	return res.json();
 }
 
 // Fetch single property
