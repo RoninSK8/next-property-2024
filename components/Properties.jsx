983c714a22b97cc31ca6b7cc2c28d801ddc7473d
Сергей Политаев <ronin@sonikelf.ru>
2024-08-16 17:52:13 +0300

    Refactor properties page: move properties list to a separate component

---
+++ components/Properties.jsx
@@ -0,0 +1,44 @@
+'use client';
+import { useState, useEffect } from 'react';
+import Spinner from './Spinner';
+import PropertyCard from './PropertyCard';
+import { fetchProperties } from '@/utils/requests';
+
+const Properties = () => {
+	const [loading, setLoading] = useState(false);
+	const [properties, setProperties] = useState([]);
+
+	useEffect(() => {
+		const getProperties = async () => {
+			const properties = await fetchProperties();
+			properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+			setProperties(properties);
+		};
+		try {
+			getProperties();
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setLoading(false);
+		}
+	}, []);
+	return loading ? (
+		<Spinner loading={loading} />
+	) : (
+		<section className="px-4 py-6">
+			<div className="container-xl lg:container m-auto px-4 py-6">
+				{properties.length === 0 ? (
+					<p>No properties found</p>
+				) : (
+					<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+						{properties.map((property) => (
+							<PropertyCard key={property._id} property={property} />
+						))}
+					</div>
+				)}
+			</div>
+		</section>
+	);
+};
+
+export default Properties;
