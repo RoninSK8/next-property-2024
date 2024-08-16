78c79fb356746a1707297ab724f2ed79ca34f315
Сергей Политаев <ronin@sonikelf.ru>
2024-08-16 20:21:12 +0300

    Add Photoswipe Lightbox Gallery

--- components/PropertyImages.jsx
+++ components/PropertyImages.jsx
@@ -1,44 +1,69 @@
 import Image from 'next/image';
+import { Gallery, Item } from 'react-photoswipe-gallery';
 
 const PropertyImages = ({ images }) => {
 	return (
-		<section className="bg-blue-50 p-4">
-			<div className="container mx-auto">
-				{images.length === 1 ? (
-					<Image
-						className="object-cover h-[400px] mx-auto rounded-xl"
-						src={images[0]}
-						alt=""
-						width={0}
-						height={0}
-						priority={true}
-					/>
-				) : (
-					<div className="grid grid-cols-2 gap-4">
-						{images.map((image, index) => (
-							<div
-								key={index}
-								className={`${
-									images.length === 3 && index === 2
-										? 'col-span-2'
-										: 'col-span-1'
-								}`}
-							>
+		<Gallery>
+			<section className="bg-blue-50 p-4">
+				<div className="container mx-auto">
+					{images.length === 1 ? (
+						<Item
+							original={images[0]}
+							thumbnail={images[0]}
+							width="1000"
+							height="600"
+						>
+							{({ ref, open }) => (
 								<Image
-									className="object-cover h-[400px] w-full rounded-xl"
-									src={image}
+									ref={ref}
+									onClick={open}
+									className="object-cover h-[400px] mx-auto rounded-xl"
+									src={images[0]}
 									alt=""
 									width={0}
 									height={0}
 									priority={true}
-									sizes="100vw"
 								/>
-							</div>
-						))}
-					</div>
-				)}
-			</div>
-		</section>
+							)}
+						</Item>
+					) : (
+						<div className="grid grid-cols-2 gap-4">
+							{images.map((image, index) => (
+								<div
+									key={index}
+									className={`${
+										images.length === 3 && index === 2
+											? 'col-span-2'
+											: 'col-span-1'
+									}`}
+								>
+									<Item
+										original={image}
+										thumbnail={image}
+										width="1000"
+										height="600"
+									>
+										{({ ref, open }) => (
+											<Image
+												ref={ref}
+												onClick={open}
+												className="object-cover h-[400px] w-full rounded-xl"
+												src={image}
+												alt=""
+												width={0}
+												height={0}
+												priority={true}
+												sizes="100vw"
+											/>
+										)}
+									</Item>
+								</div>
+							))}
+						</div>
+					)}
+				</div>
+			</section>
+		</Gallery>
 	);
 };
 
